f890a23bc5deacbdf8aa3c3498b9b1763b032d3a
Sergei Bondarev <bondarev.wt@yandex.ru>
2022-05-27 01:49:52 +0300

    Add unit tests for Lion class

---
+++ src/test/java/com/example/LionTest.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getKittensNoParamsReturnOne() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        Mockito.when(feline.getKittens()).thenReturn(1);
+        assertEquals(1,lion.getKittens());
+    }
+
+    @Test
+    public void getFoodNoParamsReturnPredatorFood() throws Exception {
+        Lion lion = new Lion("Самка", feline);
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        assertEquals(List.of("Животные", "Птицы", "Рыба"),lion.getFood());
+    }
+
+    @Test
+    public void wrongLionSexException() {
+        try {
+            Lion lion = new Lion("Гермафродит", feline);
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            assertEquals("Используйте допустимые значения пола животного - \"Самец\" или \"Самка\"", thrown.getMessage());
+        }
+    }
+}
